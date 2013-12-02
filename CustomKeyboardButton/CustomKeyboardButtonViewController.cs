26beaf33a3cad72ed2433a7b4c3a4d0f7c6fda8b
nishanil <nnish@live.com>
2013-12-02 19:51:40 +0530

    Play Input Keys

--- CustomKeyboardButton/CustomKeyboardButtonViewController.cs
+++ CustomKeyboardButton/CustomKeyboardButtonViewController.cs
@@ -16,7 +16,7 @@ namespace CustomKeyboardButton
 		{
 			// Releases the view if it doesn't have a superview.
 			base.DidReceiveMemoryWarning ();
-			
+
 			// Release any cached data, images, etc that aren't in use.
 		}
 
@@ -24,7 +24,7 @@ namespace CustomKeyboardButton
 		{
 			base.ViewDidLoad ();
 
-			UIToolbar toolbar = new UIToolbar (new RectangleF(0.0f, 0.0f, this.View.Frame.Size.Width, 44.0f));
+			MyUIToolbar toolbar = new MyUIToolbar (new RectangleF(0.0f, 0.0f, this.View.Frame.Size.Width, 44.0f));
 
 			toolbar.TintColor = UIColor.White;
 			toolbar.BarStyle = UIBarStyle.Black;
@@ -33,7 +33,7 @@ namespace CustomKeyboardButton
 
 			var myButton = new UIBarButtonItem (":-)", 
 				UIBarButtonItemStyle.Bordered, AddBarButtonText);
-		
+
 
 			toolbar.Items = new UIBarButtonItem[]{ 
 				myButton,
@@ -48,29 +48,47 @@ namespace CustomKeyboardButton
 				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
 				new UIBarButtonItem(UIBarButtonSystemItem.Done, delegate {
 					this.SampleText.ResignFirstResponder();
-			})
-			 };
+				})
+			};
 
 			this.SampleText.KeyboardAppearance = UIKeyboardAppearance.Dark;
+
+			//TODO: uncomment this if you're replacing the entire keyboard
 			//this.SampleText.InputView = toolbar;
 			this.SampleText.InputAccessoryView = toolbar;
-
-
 		}
+
+
 
 		public void AddBarButtonText(object sender, EventArgs e)
 		{
 			var barButtonItem = sender as UIBarButtonItem;
-			if (barButtonItem != null)
+			if (barButtonItem != null) {
 				this.SampleText.Text += barButtonItem.Title;
+				UIDevice.CurrentDevice.PlayInputClick ();
+			}
+
+		}
+	}
+
+	/// <summary>
+	/// Toolbar that plays input clicks
+	/// </summary>
+	public class MyUIToolbar : UIToolbar
+	{
+		public MyUIToolbar (RectangleF rect) : base(rect)
+		{
 
 		}
 
-
-
+		public override bool EnableInputClicksWhenVisible {
+			get {
+				return true;
+			}
+		}
 	}
 
-	
+
 
 }
 
